8f3876745776f730330bec2adaca061a793a8fd0
NuryanaNn <nuryan504@gmail.comescgit clear>
2021-02-24 23:18:56 +0700

    initial Commit

---
+++ class.php
@@ -0,0 +1,17 @@
+<?php
+
+// Membuat Kelas
+class CobaClass{
+
+
+
+}
+
+// Membuat Instance dari kelas tersebut
+$a = new CobaClass();
+$b = new CobaClass();
+$c = new CobaClass();
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
---
+++ constant.php
@@ -0,0 +1,34 @@
+<?php
+
+
+// define ('NAMA', 'Nuryana');
+
+
+// echo NAMA;
+
+// echo "<br>";
+
+// const KAMPUS = 23;
+
+// echo KAMPUS;
+
+
+
+// class Coba{
+// 	const NAMA = 'Nuryana';
+// }
+
+
+// echo Coba::NAMA;
+
+
+
+echo __LINE__;
+
+
+
+
+
+
+
+?>
